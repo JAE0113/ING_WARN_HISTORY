739135352afeb700a8d8fc3c15e182e1b2fb227a
SONGJAEGEON <thdworms5994@naver.com>
2026-06-15 21:21:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -30,29 +30,31 @@ function submitData() {
   };
 
   fetch(API_URL, {
-  method: "POST",
-  mode: "no-cors",
-  headers: {
-    "Content-Type": "text/plain"
-  },
-  body: JSON.stringify(data)
-})
-  .then(() => {
-    message.textContent = "저장 요청 완료";
-    message.style.color = "green";
-    result.textContent = JSON.stringify(
-      {
-        name: data.name,
-        reason: data.reason,
-        warning_date: data.warning_date
-      },
-      null,
-      2
-    );
+    method: "POST",
+    mode: "no-cors",
+    body: JSON.stringify(data)
   })
-  .catch(error => {
-    message.textContent = "요청 중 오류가 발생했습니다.";
-    message.style.color = "red";
-    console.error(error);
-  });
+    .then(() => {
+      message.textContent = "저장 요청 완료";
+      message.style.color = "green";
+
+      result.textContent = JSON.stringify(
+        {
+          name: data.name,
+          reason: data.reason,
+          warning_date: data.warning_date
+        },
+        null,
+        2
+      );
+
+      document.getElementById("pw").value = "";
+      document.getElementById("name").value = "";
+      document.getElementById("reason").value = "";
+    })
+    .catch(error => {
+      message.textContent = "요청 중 오류가 발생했습니다.";
+      message.style.color = "red";
+      console.error(error);
+    });
 }
